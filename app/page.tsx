4a1edd3c6d25e26519d1458cc09bb59f9860a9f0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-19 21:41:34 +0200

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1>first commit</h1>
+      <h1>second commit</h1>
     </main>
   );
 }
